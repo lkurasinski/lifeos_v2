c0872dc8ee43ca7d28a1d52034a3dd557af17535
lkurasin <lukasz.kurasinski@hycom.pl>
2026-05-24 14:09:18 +0200

    bugfix: 403 error on login

--- apps/web/src/lib/server/email.ts
+++ apps/web/src/lib/server/email.ts
@@ -6,6 +6,7 @@ const resend = new Resend(RESEND_API_KEY);
 const FROM_EMAIL = "LifeOS <onboarding@lifeos.lukaszkurasinski.pl>";
 
 export async function sendEmail(opts: { to: string; subject: string; html: string }) {
+	console.log(`does it send? ${FROM_EMAIL}`);
 	await resend.emails.send({
 		from: FROM_EMAIL,
 		to: opts.to,
